c888bb1c16b3d5642a1003f03a4d9949dacf67e0
sanketwakhare <sanketwakhare@gmail.com>
2023-07-09 19:15:36 +0530

    add text change when button is clicked

--- src/components/ui/button/Button.stories.js
+++ src/components/ui/button/Button.stories.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
 
 const ButtonStories = () => {
+  const [text, setText] = useState("");
+  const handleClick = (event) => {
+    setText(`${event.target.textContent} clicked`);
+  };
+
   return (
     <div>
       <h4>Buttons</h4>
-      <Button className="m10" variant="primary">
+      <Button className="m10" variant="primary" onClick={handleClick}>
         Primary
       </Button>
-      <Button className="m10" variant="secondary">
+      <Button className="m10" variant="secondary" onClick={handleClick}>
         Secondary
       </Button>
-      <Button className="m10">Default</Button>
+      <Button className="m10" onClick={handleClick}>
+        Default
+      </Button>
+
+      <span>{text}</span>
     </div>
   );
 };
