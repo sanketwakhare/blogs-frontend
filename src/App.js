856ebee2ad1c449668f4f5286c978f7666c2db1c
sanketwakhare <sanketwakhare@gmail.com>
2023-07-15 12:56:16 +0530

    add a new path for component library

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const App = () => {
       <Route path="/" element={<ComponentsStories />}></Route>
       <Route path="/article/:articleId" element={<ArticleDetails />}></Route>
       <Route path="/author/:authorId" element={<AuthorDetails />}></Route>
+      <Route path="/component-library" element={<ComponentsStories />}></Route>
     </Routes>
   );
 };
