0e8aa56622605dc3ecd3e7abf3fb69dbe2539753
sanketwakhare <sanketwakhare@gmail.com>
2023-07-15 23:37:24 +0530

    add support for password field and show hide password

--- src/components/ui/text-field/TextField.js
+++ src/components/ui/text-field/TextField.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TextField.scss";
+import Button from "../button/Button";
 
 const TextField = (props) => {
-  let { className, ...rest } = props;
+  let { className, passwordField, showHidePassword, type, ...rest } = props;
 
   let txtFieldClasses = `text-field ${className}`;
+  if (passwordField) {
+    type = "password";
+  }
 
-  return <input className={txtFieldClasses} {...rest}></input>;
+  const [fieldType, setFieldType] = useState(type);
+
+  const togglePassword = () => {
+    setFieldType((prevFieldType) => {
+      return prevFieldType === "password" ? "text" : "password";
+    });
+  };
+
+  return (
+    <>
+      <input className={txtFieldClasses} type={fieldType} {...rest}></input>
+      {passwordField && showHidePassword && (
+        <Button className="btn-password-field" onClick={() => togglePassword()}>
+          {fieldType === "password" ? (
+            <i className="fa fa-eye-slash"></i>
+          ) : (
+            <i className="fa fa-eye"></i>
+          )}
+        </Button>
+      )}
+    </>
+  );
 };
 
 export default TextField;
--- src/components/ui/text-field/TextField.stories.js
+++ src/components/ui/text-field/TextField.stories.js
@@ -5,11 +5,18 @@ const TextFieldStories = () => {
   return (
     <div>
       <h4>Text Fields</h4>
-      <TextField className="m10 w200" placeholder="Search"></TextField>
+      <TextField className="m10 w300" placeholder="Search"></TextField>
       <TextField
         className="m10 w300"
         value="disabled text field"
         disabled
+      ></TextField>
+      <TextField
+        className="w300"
+        placeholder="Password"
+        type="password"
+        passwordField="true"
+        showHidePassword="true"
       ></TextField>
     </div>
   );
