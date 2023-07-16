a6d6143322677d93053dbd553e9958e35b4fa97d
sanketwakhare <43931717+sanketwakhare@users.noreply.github.com>
2023-07-16 11:31:23 +0530

    Component lib changes (#10)
    * make button themeable
    
    * add theme color variables
    for disabled and placeholder
    
    * add support for password field
    and show hide password
    
    * make textfield themeable
    
    * add left border for toasts
    
    * make chips themeable
    
    * implement toggle theme feature
    
    * refactor

---
+++ src/components/ui/theme-switcher/ThemeSwitcher.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Button from '../button/Button';
+import './ThemeSwitcher.scss';
+
+const ThemeTypes = {
+    LIGHT: 'light',
+    DARK: 'dark',
+}
+
+const ThemeSwitcher = (props) => {
+
+    const { LIGHT, DARK } = ThemeTypes;
+
+    const { initialTheme = LIGHT, className, ...rest } = props;
+    const [theme, setTheme] = React.useState(initialTheme);
+
+    const toggleTheme = () => {
+        setTheme((theme) => (theme === LIGHT ? DARK : LIGHT));
+        const root = document.querySelector("html");
+        const classes = root.classList;
+        if (classes.contains(DARK)) {
+            classes.remove(DARK);
+            classes.add(LIGHT);
+        } else {
+            classes.remove(LIGHT);
+            classes.add(DARK);
+        }
+    };
+
+    const themeIconClasses = ["fa"];
+    themeIconClasses.push(theme === LIGHT ? "fa-moon" : "fa-sun");
+
+    const themeBtnClasses = ["btn-theme-switcher"];
+    themeBtnClasses.push(className);
+
+    return (
+        <Button onClick={() => toggleTheme()} className={themeBtnClasses.join(" ")} {...rest}>
+            <i className={themeIconClasses.join(" ")}></i>
+        </Button>
+    )
+}
+
+export default ThemeSwitcher;
--- src/stories/Components.stories.js
+++ src/stories/Components.stories.js
@@ -1,12 +1,12 @@
-import React from "react";
+import ArticleDetailsStories from "src/components/article/article-details/ArticleDetails.stories";
+import ArticleSummaryCardStories from "src/components/article/article-summary/ArticleSummaryCard.stories";
 import ButtonStories from "src/components/ui/button/Button.stories";
+import ChipsStories from "src/components/ui/chip/Chips.stories";
 import ModalStories from "src/components/ui/modal/Modal.stories";
 import ModalContainer from "src/components/ui/modal/ModalContainer.stories";
 import NavBarStories from "src/components/ui/navbar/NavBar.stories";
 import TextFieldStories from "src/components/ui/text-field/TextField.stories";
-import ArticleSummaryCardStories from "src/components/article/article-summary/ArticleSummaryCard.stories";
-import ArticleDetailsStories from "src/components/article/article-details/ArticleDetails.stories";
-import ChipsStories from "src/components/ui/chip/Chips.stories";
+import ThemeSwitcherStories from "src/components/ui/theme-switcher/ThemeSwitcher.stories";
 import ToastStories from "src/components/ui/toast/Toast.stories";
 
 const ComponentsStories = () => {
@@ -28,6 +28,8 @@ const ComponentsStories = () => {
       <ChipsStories />
       <hr />
       <ToastStories />
+      <hr />
+      <ThemeSwitcherStories />
       <hr />
       <ArticleSummaryCardStories />
       <hr />
